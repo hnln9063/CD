730964dc2bf5617dd1110acf1d2f54beeb0d119a
H N Lakshmi Narasimha <82299708+hnln9063@users.noreply.github.com>
2023-04-28 18:33:10 +0530

    Added Compiler Design programs

---
+++ Exp1.c
@@ -0,0 +1,33 @@
+hnln@DESKTOP-2JC785T:~/CD$ cat kar.c
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    printf("Enter the string over the alphabets (a, b, c): ");
+    scanf("%s", str);
+    int q = 0, prev_q;
+    for (int i = 0; i < strlen(str); i++) {
+        if(str[i]!='a' && str[i]!='b' && str[i]!='c'){
+            printf("Invalid string!!\nString is not accepted.\n");
+            return 0;
+        }
+        prev_q = q;
+        switch(q) {
+            case 0 : q = (str[i] == 'a') ? 1 : 0;
+                     break;
+            case 1 : q = (str[i] == 'b') ? 2 : (str[i] == 'a') ? 1 : 0;
+                     break;
+            case 2 : q = (str[i] == 'c') ? 3 : (str[i] == 'a') ? 1 : 0;
+                     break;
+            case 3 : q = (str[i] == 'a') ? 1 : 0;
+                     break;
+        }
+        printf("%c : q%d-->q%d\n", str[i], prev_q, q);
+    }
+    if (q == 3)
+        printf("Given string reached final state q3, so it is Accepted.\n");
+    else
+        printf("Given string is in state q%d, so it is not accepted.\n",q);
+    return 0;
+}
---
+++ Exp2.c
@@ -0,0 +1,85 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+void ELS(char s[])
+{
+	int not=0,i,f=0,j,flag=0;
+	for(i=0;i<strlen(s);i++)
+		if(s[i]=='|')
+			not++;
+	char s1[not+1][100];
+	int idx1=0,idx2=0;
+	int count[not+1],c=0;
+
+	for(i=3;i<strlen(s);i++) {
+		if(s[i]=='|') {
+			idx2=0;
+			count[idx1]=c;
+			c=0;
+			idx1++;
+		} else {
+			s1[idx1][idx2]=s[i];
+			c++;
+			idx2=idx2+1;
+		}
+	}
+
+	count[idx1]=c;
+	for(i=0;i<=not;i++) {
+		if(s[0]==s1[i][0])
+		flag=1;
+	}
+	if(flag==1) {
+		printf("After elemination of left recursion \n");
+		printf("%c->",s[0]);
+		for(i=0;i<=not;i++) {
+			if(s[0]!=s1[i][0]) {
+				if(f==0) {
+					f=1;
+					for(j=0;j<count[i];j++)
+						printf("%c",s1[i][j]);	
+					printf("%c'",s[0]);
+				} else {
+					printf("|");
+					for(j=0;j<count[i];j++)
+						printf("%c",s1[i][j]);
+					printf("%c'",s[0]);
+				}
+			}
+		}
+		printf("\n");
+		printf("%c'->",s[0]);
+		f=0;
+		for(i=0;i<=not;i++) {
+			if(s[0] == s1[i][0]) {
+				if(f == 0) {
+					f = 1;
+					for(j=1;j<count[i];j++)
+						printf("%c",s1[i][j]);
+					printf("%c'",s[0]);
+				} else {
+					printf("|");
+					for(j=1;j<count[i];j++)
+					printf("%c",s1[i][j]);
+					printf("%c'",s[0]);
+				}
+			}
+		}
+		printf("|E\n");
+	} else
+		printf("Given production %s is not in left recursion\n",s);
+}
+void main()
+{
+	char s[100];
+	int n;
+	printf("Enter no of productions : ");
+	scanf("%d",&n);
+	while(n--!=0)
+	{
+		printf("Ener the Production in the form(A->A@|B) : ");
+		scanf("%s",s);
+		printf("The Production is : %s\n",s);
+		ELS(s);
+	}
+}
